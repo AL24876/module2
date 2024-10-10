dcb8f505935372aacbdf2ebc0b0932ba7ae2f213
AL24876 <Fear76tm@mail.ru>
2024-10-10 15:16:05 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,14 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+k = 0
+while k < len(my_list):
+    if my_list[k] > 0:
+        print(my_list[k])
+        k += 1
+    elif my_list[k] == 0:
+        k += 1
+        continue
+    elif my_list[-k] < 0:
+        k += 1
+        continue
+    else:
+        k += 1
